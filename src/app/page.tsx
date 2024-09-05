790464c7df427f77b393acfb2c607b51160ecb98
jniac <joseph.merdrignac@gmail.com>
2024-09-05 16:31:05 +0200

    just another attempt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -12,6 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1>OOOOOK</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -97,5 +98,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
